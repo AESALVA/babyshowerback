43c8e360a3221e25a65774c9a18edfdb3f2e3f92
AESalva <eduardo_salva@hotmail.com>
2023-04-07 20:21:43 -0300

    test users with postman

--- Routes/users.js
+++ Routes/users.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 const User = require("../Models/User");
 
-
+router
+  .get("/all", async (req, res) => {
+    console.log("GET /users/all");
+    let Users = [];
+    try {
+      const allUsers = await User.find();
+      allUsers.map((users) => {
+        user = { name: users.name, role: users.role, mail:users.mail };
+        Users = [...Users, user];
+      });
+      res.status(200).json(Users);
+    } catch (error) {
+      res.status(400).json({ error: true, message: error });
+    }
+  })
 
 
 
