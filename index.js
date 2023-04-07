38fe6740690a4f1da4a066767c3681ab2fa591e6
AESalva <eduardo_salva@hotmail.com>
2023-04-07 19:44:11 -0300

    structures database features

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 app.use(express.json());
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 let cors = require('cors')
 app.use(cors({origin:"*"}));
+
+mongoose.connect(process.env.URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+  }).then(()=>{console.log('database connection OK')}).catch((error)=>{console.error(error)});
 
 
 
@@ -15,5 +22,4 @@ const commentsRoutes = require('./Routes/comments');
 app.use('/comments', commentsRoutes)
 
 app.listen(PORT, ()=>{console.log('server running on port '+ PORT )});
-
 
