b5613bc16e25abc8355e9980324c6ce7907479fa
AESalva <eduardo_salva@hotmail.com>
2023-04-08 10:27:54 -0300

    features commets route

--- Routes/comments.js
+++ Routes/comments.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 const Comment = require("../Models/Comment");
 
+router
+  .get("/all", async (req, res) => {
+    console.log("GET /comments/all");
 
+    try {
+      const allComments = await Comment.find();
+      res.status(200).json(allComments);
+    } catch (error) {
+      res.status(400).json({ error: true, message: error });
+    }
+  })
 
 
 module.exports = router;
