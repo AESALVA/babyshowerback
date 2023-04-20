7a596b0497d112c48578576f2ef7e72e942127c3
AESalva <eduardo_salva@hotmail.com>
2023-04-20 18:01:49 -0300

    features comments route

--- Routes/comments.js
+++ Routes/comments.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Comment = require("../Models/Comment");
+const { body, validationResult } = require('express-validator');
+
 
 router
   .get("/all", async (req, res) => {
@@ -8,6 +10,23 @@ router
     try {
       const allComments = await Comment.find();
       res.status(200).json(allComments);
+    } catch (error) {
+      res.status(400).json({ error: true, message: error });
+    }
+  }).post("/newMessage", body("message").matches("^[a-zA-Z ]*$").isLength({ min: 8, max: 300 }), async (req, res) => {
+    console.log("POST /Messages/newMessage");
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array(), message:"validation error" });
+    }
+
+    const { body } = req;
+
+    try {
+      const newMessage = new Comment(body);
+      await newMessage.save();
+      res.status(200).json(newMessage);
     } catch (error) {
       res.status(400).json({ error: true, message: error });
     }
